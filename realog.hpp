695ac3ae96f57b531f504ecf8f2d1f0de6fc0f08
derickfelix <derickfelix4321@gmail.com>
2020-05-02 17:44:00 -0400

    Add color and levels

--- realog.hpp
+++ realog.hpp
@@ -1,3 +1,26 @@
+/*
+  MIT License
+
+  Copyright (c) 2020 Derick Felix
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
 #ifndef __Realog_Header__
 #define __Realog_Header__
 
@@ -59,11 +82,16 @@ namespace realog {
     }
   };
 
+  enum class Level
+  {
+    TRACE, DEBUG, INFO, WARN, ERROR, CRITICAL
+  };
+
   class Logger
   {
   public:
-    Logger(const std::string &name = "Realog")
-    : m_name(name)
+    Logger(const std::string &name = "Realog", const Level &level = Level::TRACE)
+    : m_name(name), m_level(level)
     {
     }
 
@@ -71,16 +99,79 @@ namespace realog {
     Logger &operator=(const Logger &other) = delete;
 
     template<typename... Args>
-    void info(const std::string &message, const Args&... args)
+    inline void trace(const std::string &message, const Args&... args)
     {
-      log(message, Util::convertArgsToStringVector(args...));
+      log(message, Level::TRACE, Util::convertArgsToStringVector(args...));
+    }
+
+    template<typename... Args>
+    inline void debug(const std::string &message, const Args&... args)
+    {
+      log(message, Level::DEBUG, Util::convertArgsToStringVector(args...));
+    }
+
+    template<typename... Args>
+    inline void info(const std::string &message, const Args&... args)
+    {
+      log(message, Level::INFO, Util::convertArgsToStringVector(args...));
+    }
+
+    template<typename... Args>
+    inline void warn(const std::string &message, const Args&... args)
+    {
+      log(message, Level::WARN, Util::convertArgsToStringVector(args...));
+    }
+
+    template<typename... Args>
+    inline void error(const std::string &message, const Args&... args)
+    {
+      log(message, Level::ERROR, Util::convertArgsToStringVector(args...));
+    }
+
+    template<typename... Args>
+    inline void critical(const std::string &message, const Args&... args)
+    {
+      log(message, Level::CRITICAL, Util::convertArgsToStringVector(args...));
+    }
+
+    template<typename... Args>
+    inline void log(const std::string &message, const Args&... args)
+    {
+      log(message, m_level, Util::convertArgsToStringVector(args...));
+    }
+
+    inline void setLevel(const Level &level)
+    {
+      m_level = level;
     }
 
   private:
-    void log(const std::string &message, const std::vector<std::string> &args)
+    void log(const std::string &message, const Level& level, const std::vector<std::string> &args)
     {
       time_t now = time(0);
       tm *ltm = localtime(&now);
+
+      switch (level)
+      {
+      case Level::TRACE:
+        std::cout << "\033[0m";
+        break;
+      case Level::DEBUG:
+        std::cout << "\033[0;34m";
+        break;
+      case Level::INFO:
+        std::cout << "\033[0;32m";
+        break;
+      case Level::WARN:
+        std::cout << "\033[0;33m";
+        break;
+      case Level::ERROR:
+        std::cout << "\033[0;31m";
+        break;
+      case Level::CRITICAL:
+        std::cout << "\033[0;41m";
+        break;
+      };
 
       auto result = build(message, args);
 
@@ -89,7 +180,9 @@ namespace realog {
       << (ltm->tm_min < 10 ? "0" : "") << ltm->tm_min << ":" 
       << (ltm->tm_sec < 10 ? "0" : "") << ltm->tm_sec << "] " 
       << m_name << ": "
-      << result << std::endl;
+      << result;
+
+      std::cout << "\033[0m" << std::endl;
     }
 
     std::string build(const std::string &message, const std::vector<std::string> &args)
@@ -107,6 +200,7 @@ namespace realog {
 
   private:
     std::string m_name;
+    Level m_level;
   };
 }
 
--- test.cpp
+++ test.cpp
@@ -1,3 +1,26 @@
+/*
+  MIT License
+
+  Copyright (c) 2020 Derick Felix
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
 #include "realog.hpp"
 
 int main()
@@ -5,18 +28,33 @@ int main()
   std::cout << "SIMPLE EXAMPLE\n";
   {
     realog::Logger logger("My Logger");
-    logger.info("Hello my Logger!");
+    logger.log("Hello my Logger!");
   }
 
-  std::cout << "\n\nVARIABLES EXAMPLE\n";
+  std::cout << "\nVARIABLES EXAMPLE\n";
   {
     realog::Logger logger;
 
-    logger.info("This is {0} triangle with {1} Custom {0} vars={1} * {2}", "A", 20, 40 * 20);
+    logger.log("This is {0} triangle with {1} Custom {0} vars={1} * {2}", "A", 20, 40 * 20);
     
-    logger.info("I {0} am {1} using {2} more {3} than {4} I {5} need", "_", 'A', 40.2 / 30, 10.0 / 3);
+    logger.log("I {0} am {1} using {2} more {3} than {4} I {5} need", "_", 'A', 40.2 / 30, 10.0 / 3);
 
-    logger.info("Number {0} times number {1} is {2}", 40.2, 24, 40.2 * 24);
+    logger.log("Number {0} times number {1} is {2}", 40.2, 24, 40.2 * 24);
+  }
+
+  std::cout << "\nCOLOR AND LEVELS\n";
+  {
+    realog::Logger logger("MyLogger", realog::Level::INFO);
+    logger.log("Here is my info!");
+    logger.setLevel(realog::Level::WARN);
+    logger.log("Changed to warning!");
+    std::cout << "\n";
+    logger.trace("Show Trace");
+    logger.debug("Show Debug");
+    logger.info("Show Info");
+    logger.warn("Show Warning");
+    logger.error("Show Error");
+    logger.critical("Show Critical");
   }
 
   return 0;
