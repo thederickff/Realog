0b8a17f66ec746a3618dd0bdcbcfbe3bfb5a90f6
derickfelix <derickfelix4321@gmail.com>
2020-05-02 17:02:02 -0400

    Finish variables implementation

--- realog.hpp
+++ realog.hpp
@@ -7,17 +7,35 @@
 #include <vector>
 
 namespace realog {
+  struct NoArg {};
+
+  std::ostream &operator<<(std::ostream &stream, const NoArg& arg)
+  {
+    return stream;
+  }
+
   class Util
   {
   public:
     Util() = delete;
+
+    static void replaceAll(std::string &string, const std::string &replace, const std::string &with)
+    {
+      size_t pos = string.find(replace);
+
+      while (pos != std::string::npos)
+      {
+        string.replace(pos, replace.size(), with);
+        pos = string.find(replace, pos + with.size());
+      }
+    }
 
     template<typename... Args>
     static std::vector<std::string> convertArgsToStringVector(const Args&... args)
     {
       std::vector<std::string> vector;
 
-      iterate(vector, NULL, args...);
+      iterate(vector, NoArg(), args...);
 
       return vector;
     }
@@ -33,7 +51,7 @@ namespace realog {
     template<typename T>
     static void iterate(std::vector<std::string> &vector, T t)
     {
-      if (reinterpret_cast<const void*>(t) != NULL) {
+      if (!std::is_same<T, NoArg>::value) {
         std::stringstream stream;
         stream << t;
         vector.push_back(stream.str());
@@ -53,13 +71,13 @@ namespace realog {
     Logger &operator=(const Logger &other) = delete;
 
     template<typename... Args>
-    void info(const char* message, const Args&... args)
+    void info(const std::string &message, const Args&... args)
     {
       log(message, Util::convertArgsToStringVector(args...));
     }
 
   private:
-    void log(const char* message, const std::vector<std::string> &args)
+    void log(const std::string &message, const std::vector<std::string> &args)
     {
       time_t now = time(0);
       tm *ltm = localtime(&now);
@@ -74,24 +92,19 @@ namespace realog {
       << result << std::endl;
     }
 
-    std::string build(const char* message, const std::vector<std::string> &args)
+    std::string build(const std::string &message, const std::vector<std::string> &args)
     {
-      std::cout << "MESSAGE: " << message << std::endl;    
+      std::string result = message;
 
-      if (args.size() > 0) {
+      for (size_t i = 0; i < args.size(); ++i) {
         std::stringstream ss;
-
-        for (const std::string &arg : args) {
-          std::cout << arg << " ";
-        }
-
-        std::cout << std::endl;
+        ss << '{' << i << '}';
+        Util::replaceAll(result, ss.str(), args[i]);
       }
       
-      return message;
+      return result;
     }
 
-   
   private:
     std::string m_name;
   };
--- test.cpp
+++ test.cpp
@@ -2,11 +2,22 @@
 
 int main()
 {
-  realog::Logger logger;
-  logger.info("Hello My Logger");
+  std::cout << "SIMPLE EXAMPLE\n";
+  {
+    realog::Logger logger("My Logger");
+    logger.info("Hello my Logger!");
+  }
 
-  realog::Logger custom("Custom");
-  custom.info("Hello My Custom var={0}", 2, "A hello", 20, 20, "I don't know", 30, 31);
+  std::cout << "\n\nVARIABLES EXAMPLE\n";
+  {
+    realog::Logger logger;
+
+    logger.info("This is {0} triangle with {1} Custom {0} vars={1} * {2}", "A", 20, 40 * 20);
+    
+    logger.info("I {0} am {1} using {2} more {3} than {4} I {5} need", "_", 'A', 40.2 / 30, 10.0 / 3);
+
+    logger.info("Number {0} * {1} = {2}", 40.2, 24, 40.2 * 24);
+  }
 
   return 0;
 }
